2fb00ca00d0b126656cebf58f57d5390a5123637
chaukha <lechaukha12@gmail.com>
2021-03-21 21:48:49 +0700

    Add files via upload

---
+++ Tai ram.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+def lewlew():
+	messagebox.showinfo('Warning','Bi Encrypted roi nha Lew Lewwwww')
+chaukha = Tk()
+chaukha.title('Tai Ram')
+#Tao control1
+control1 = Label(chaukha, text='Chao mung den voi phan mem tai Ram', font='Arial',background='blue').pack()
+#Tao button
+lckbutton = Button(chaukha,text='Download',font='Arial',command=lewlew).pack()
+chaukha.mainloop()
